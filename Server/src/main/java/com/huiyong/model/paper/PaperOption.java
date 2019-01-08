67d0fde3aaba202c1948d9766668eef1a75c8d0b
gangpu <pugang1985@github.com>
2019-01-08 14:25:51 +0800

    return option point to client

--- Server/src/main/java/com/huiyong/model/paper/PaperOption.java
+++ Server/src/main/java/com/huiyong/model/paper/PaperOption.java
@@ -10,6 +10,7 @@ package com.huiyong.model.paper;
 public class PaperOption {
 	private Integer id;
 	private String description;
+	private Integer optionPoint;
 	public Integer getId() {
 		return id;
 	}
@@ -21,5 +22,17 @@ public class PaperOption {
 	}
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	/**
+	 * @return the optionPoint
+	 */
+	public Integer getOptionPoint() {
+		return optionPoint;
+	}
+	/**
+	 * @param optionPoint the optionPoint to set
+	 */
+	public void setOptionPoint(Integer optionPoint) {
+		this.optionPoint = optionPoint;
 	}
 }
